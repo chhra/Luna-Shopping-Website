f6d81bfbd55612245213a44f1863e358ba3b332b
Chahrazed Bousmaha <bousmahachahrazed2906@gmail.com>
2026-06-26 23:15:21 +0100

    change port for gmail

--- backend/controller/mail.controller.js
+++ backend/controller/mail.controller.js
@@ -9,8 +9,8 @@ export const sendOrder = async (req, res) => {
     // set up the email sender (using Gmail as an example)
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
