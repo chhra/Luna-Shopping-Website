e669fa24fea481666acbd10df24c12f0494332a2
Chahrazed Bousmaha <bousmahachahrazed2906@gmail.com>
2026-06-26 20:48:35 +0100

    -M

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "../config/database.js";
-import app from "./app.js";
+import app from "./App.js";
 import productRoutes from "../routes/product.route.js";
 import dns from "dns";
 //change DNS
